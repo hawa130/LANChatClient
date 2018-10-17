c822a65fa76ae36e5d85a2421a697d740f847b46
hawa130 <handanwanga@hotmail.com>
2018-10-17 16:21:54 +0800

    Added sidebar

--- chatClient.py
+++ chatClient.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import re
 import sys
 import time
 import socket
@@ -13,124 +14,124 @@ global port
 global myaddr
 
 s = socket.socket()
-
 dic = {}
+IPpattern = r'\(((25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\)'
 
-def isCmd(msg):
-	if msg[0] == '/':
-		return True
-	else:
-		return False
+class processText(object):
+	def __init__(self):
+		super(processText, self).__init__()
+		self.arg = arg
 
-def parseCmd(cmd):
-	if cmd[:7] == '/sendby':
-		cmd = cmd[8:]
-		ip = cmd.split(' ')[0]
-		cmd = cmd.replace(ip + ' ', '', 1)
-		cmd = cmd.replace('&', '&amp;')
-		cmd = cmd.replace('>', '&gt;')
-		cmd = cmd.replace('<', '&lt;')
-		cmd = cmd.replace('"','&quot;')
-		cmd = cmd.replace("'",'&#39;')
-		cmd = cmd.replace(' ','&nbsp;')
-		cmd = cmd.replace('\n','<br>')
-		cmd = cmd.replace('\r','<br>')
+	def isCmd(msg):
+		if msg[0] == '/':
+			return True
+		else:
+			return False
+
+	def parseCmd(cmd):
+		if cmd[:7] == '/sendby':
+			cmd = cmd[8:]
+			ip = cmd.split(' ')[0]
+			cmd = cmd.replace(ip + ' ', '', 1)
+			cmd = cmd.replace('&', '&amp;')
+			cmd = cmd.replace('>', '&gt;')
+			cmd = cmd.replace('<', '&lt;')
+			cmd = cmd.replace('"','&quot;')
+			cmd = cmd.replace("'",'&#39;')
+			cmd = cmd.replace(' ','&nbsp;')
+			cmd = cmd.replace('\n','<br>')
+			cmd = cmd.replace('\r','<br>')
+			try:
+				name = dic[ip]
+			except KeyError:
+				name = ip
+			nowTime = datetime.datetime.now().strftime('%H:%M:%S')
+			return ('<font color="gray">' + name + '(' + ip + ') speaks to you quietly at ' + nowTime + ' :</font><br><font color="black">' + cmd + '</font>')
+		else:
+			opt = cmd.split(' ')
+			if opt[0] == '/login':
+				if opt[1] == 'true':
+					ret = 'Logged in!'
+				elif opt[1] == 'false':
+					ret = 'Failed. Please check your information and try again.'
+			elif opt[0] == '/admin':
+				if opt[1] == 'true':
+					ret = 'Logged in as administrator.'
+				elif opt[1] == 'false':
+					ret = 'Failed.'
+			elif opt[0] == '/newUser' or opt[0] == '/changeName' or opt[0] == '/delUser':
+				return cmd
+		return '<i><font color="gray">' + ret + '</font></i>'
+
+	def parseText(msg):
+		msg = msg[6:]
+		ip = msg.split(' ')[0]
+		msg = msg.replace(ip + ' ', '', 1)
+		msg = msg.replace('&', '&amp;')
+		msg = msg.replace('>', '&gt;')
+		msg = msg.replace('<', '&lt;')
+		msg = msg.replace('"','&quot;')
+		msg = msg.replace("'",'&#39;')
+		msg = msg.replace(' ','&nbsp;')
+		msg = msg.replace('\n','<br>')
+		msg = msg.replace('\r','<br>')
 		try:
 			name = dic[ip]
 		except KeyError:
 			name = ip
 		nowTime = datetime.datetime.now().strftime('%H:%M:%S')
-		return ('<font color="gray">' + name + '(' + ip + ') speaks to you quietly at ' + nowTime + ' :</font><br><font color="black">' + cmd + '</font>')
-	cmd = cmd.split(' ')
-	if cmd[0] == '/login':
-		if cmd[1] == 'true':
-			return ('Logged in!')
-		elif cmd[1] == 'false':
-			return ('Failed. Please check your information and try again.')
-	elif cmd[0] == '/admin':
-		if cmd[1] == 'true':
-			return ('Logged in as administrator.')
-		elif cmd[1] == 'false':
-			return ('Failed.')
-	elif cmd[0] == '/newUser':
-		dic[cmd[1]] = cmd[2]
-		return ('User ' + cmd[2] + '(' + cmd[1] + ')' + ' joined in the chatting.')
-	elif cmd[0] == '/changeName':
+		if ip == myaddr:
+			return ('<font color="green">' + name + '(' + ip + ') ' + nowTime + '</font><br>' + msg)
+		return ('<font color="blue">' + name + '(' + ip + ') ' + nowTime + '</font><br>' + msg)
+
+	def connect(host, port):
+		s.connect((host, port))
+		nowTime = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+		print('Connected to ' + host + ':' + str(port) + ' at ' + nowTime)
+
+	def send(self, msg):
+		if msg == '':
+			return
+		if msg == '/quit':
+			s.close()
+		if not self.isCmd(msg):
+			msg = '/text ' + msg
+		elif msg[:6] == '/login':
+			msg = msg[7:].split(' ')
+			password = hashlib.md5(msg[1].encode('utf-8')).hexdigest()
+			msg = '/login ' + msg[0] + ' ' + password
+		elif msg[:12] == '/setPassword':
+			msg = '/setPassword ' + hashlib.md5(msg[13:].encode('utf-8')).hexdigest()
+		elif msg[:8] == '/setName':
+			msg = msg[9:]
+			if re.search(IPpattern, msg):
+				return
+			elif len(msg) > 20:
+				msg = msg[:20]
+			msg = '/setName ' + msg
 		try:
-			oldName = dic[cmd[1]]
-		except KeyError:
-			oldName = cmd[1]
-		ret = ('User ' + oldName + '(' + cmd[1] + ')' + ' has changed the name into ' + cmd[2] + '.')
-		dic[cmd[1]] = cmd[2]
-		return ret
-	elif cmd[0] == '/delUser':
+			s.send(msg.encode('GBK'))
+		except ConnectionResetError:
+			print('Cannot connect to the server.')
+
+	def receive(self):
 		try:
-			name = dic[cmd[1]]
-		except KeyError:
-			return ('User ' + cmd[1] + ' quitted the chatting.')
-		ret = ('User ' + name + '(' + cmd[1] + ') quitted the chatting.')
-		del dic[cmd[1]]
-		return ret
-
-def parseText(msg):
-	msg = msg[6:]
-	ip = msg.split(' ')[0]
-	msg = msg.replace(ip + ' ', '', 1)
-	msg = msg.replace('&', '&amp;')
-	msg = msg.replace('>', '&gt;')
-	msg = msg.replace('<', '&lt;')
-	msg = msg.replace('"','&quot;')
-	msg = msg.replace("'",'&#39;')
-	msg = msg.replace(' ','&nbsp;')
-	msg = msg.replace('\n','<br>')
-	msg = msg.replace('\r','<br>')
-	try:
-		name = dic[ip]
-	except KeyError:
-		name = ip
-	nowTime = datetime.datetime.now().strftime('%H:%M:%S')
-	if ip == myaddr:
-		return ('<font color="green">' + name + '(' + ip + ') ' + nowTime + '</font><br>' + msg)
-	return ('<font color="blue">' + name + '(' + ip + ') ' + nowTime + '</font><br>' + msg)
-
-def connect(host, port):
-	s.connect((host, port))
-	nowTime = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-	print('Connected to ' + host + ':' + str(port) + ' at ' + nowTime)
-
-def send(msg):
-	if msg == '':
-		return
-	if msg == '/quit':
-		s.close()
-	if not isCmd(msg):
-		msg = '/text ' + msg
-	elif msg[:6] == '/login':
-		msg = msg[7:].split(' ')
-		password = hashlib.md5(msg[1].encode('utf-8')).hexdigest()
-		msg = '/login ' + msg[0] + ' ' + password
-	elif msg[:12] == '/setPassword':
-		msg = '/setPassword ' + hashlib.md5(msg[13:].encode('utf-8')).hexdigest()
-	s.send(msg.encode('GBK'))
-
-def receive():
-	try:
-		msg = s.recv(1024).decode('GBK')
-	except ConnectionResetError:
-		print('Cannot connect to the server.')
-		time.sleep(5)
-		return
-	if msg == None:
-		return
-	data = msg.split('\n')
-	if msg[:5] == '/text':
-		return parseText(msg)
-	for msg in data:
-		if msg[:7] == '/server':
-			nowTime = datetime.datetime.now().strftime('%H:%M:%S')
-			return ('Server ' + nowTime + '\n' + msg[8:])
-		elif msg[0] == '/':
-			return '<font color="gray">' + parseCmd(msg) + '</font>'
+			msg = s.recv(1024).decode('GBK')
+		except ConnectionResetError:
+			print('Cannot connect to the server.')
+			time.sleep(5)
+			return
+		if msg == None:
+			return
+		data = msg.split('\n')
+		if msg[:5] == '/text':
+			return self.parseText(msg)
+		for msg in data:
+			if msg[:7] == '/server':
+				nowTime = datetime.datetime.now().strftime('%H:%M:%S')
+				return ('<b><font color="gray">Server ' + nowTime + '</font></b><br>' + msg[8:])
+			elif msg[0] == '/':
+				return self.parseCmd(msg)
 
 class MyThread(QtCore.QThread):
 	trigger = QtCore.pyqtSignal(str)
@@ -140,8 +141,7 @@ class MyThread(QtCore.QThread):
 
 	def run(self):
 		while True:
-			message = receive()
-			print(message)
+			message = processText.receive(processText)
 			self.trigger.emit(message)
 
 class Ui_MainWindow(object):
@@ -222,7 +222,6 @@ class Ui_MainWindow(object):
 		self.retranslateUi(MainWindow)
 		self.sendButton.clicked.connect(self.TextArea.setFocus)
 		self.sendButton.clicked.connect(self.sendMessage)
-		#self.sendButton.clicked.connect(self.TextArea.clear)
 		self.sendButton.clicked.connect(MainWindow.sendStatus)
 		self.TextArea.cursorPositionChanged.connect(MainWindow.initStatusbar)
 		QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -231,11 +230,10 @@ class Ui_MainWindow(object):
 		MainWindow.setTabOrder(self.textBrowser, self.IPlist)
 
 		self.initStatusbar()
-		self.IPlist.itemClicked.connect(self.selectIP)
+		self.IPlist.itemDoubleClicked.connect(self.selectIP)
 		self.threads = MyThread(self)
 		self.threads.trigger.connect(self.updateText)
 		self.threads.start()
-		#self.IPlist.addItems(['qaq', 'qwq'])
 
 	def retranslateUi(self, MainWindow):
 		_translate = QtCore.QCoreApplication.translate
@@ -250,8 +248,39 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
 		super(Window, self).__init__()
 		self.setupUi(self)
 
+	def parseCmd(self, cmd):
+		opt = cmd.split(' ')
+		if opt[0] == '/newUser':
+			dic[opt[1]] = opt[2]
+			self.addToList(opt[2] + '(' + opt[1] + ')')
+			ret = 'User ' + opt[2] + '(' + opt[1] + ')' + ' joined in the chatting.'
+			return '<font color="gray">' + ret + '</font>'
+		elif opt[0] == '/changeName':
+			try:
+				oldName = dic[opt[1]]
+			except KeyError:
+				oldName = opt[1]
+			self.updateList(oldName + '(' + opt[1] + ')', opt[2] + '(' + opt[1] + ')')
+			ret = ('User ' + oldName + '(' + opt[1] + ')' + ' has changed the name into ' + opt[2] + '.')
+			dic[opt[1]] = opt[2]
+			return '<font color="gray">' + ret + '</font>'
+		elif opt[0] == '/delUser':
+			try:
+				name = dic[opt[1]]
+			except KeyError:
+				ret = 'User ' + opt[1] + ' quitted the chatting.'
+			else:
+				ret = 'User ' + name + '(' + opt[1] + ') quitted the chatting.'
+				del dic[opt[1]]
+			self.delFromList(name + '(' + opt[1] + ')')
+			return '<font color="gray">' + ret + '</font>'
+		else:
+			return cmd
+
 	def selectIP(self, item):
-		self.TextArea.appendPlainText(item.text() + ' ')
+		ip = item.text()
+		ip = re.sub(IPpattern, '', ip)
+		self.TextArea.append('/sendto ' + ip + ' ')
 		self.TextArea.setFocus()
 	
 	def initStatusbar(self):
@@ -263,11 +292,13 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
 
 	def sendMessage(self):
 		message = self.TextArea.toPlainText()
-		send(message)
+		processText.send(processText, message)
 		self.TextArea.clear()
 
 	def updateText(self, message):
+		message = self.parseCmd(message)
 		self.textBrowser.append(message)
+		print(message)
 		self.textBrowser.moveCursor(QtGui.QTextCursor.End)
 
 	def keyPressEvent(self, event):
@@ -275,6 +306,19 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
 			if str(event.key()) == '16777221' or str(event.key()) == '16777220':
 				self.sendMessage()
 				self.sendStatus()
+
+	def addToList(self, item):
+		self.IPlist.addItem(item)
+
+	def delFromList(self, string):
+		items = self.IPlist.findItems(string, QtCore.Qt.MatchExactly)
+		for item in items:
+			self.IPlist.takeItem(self.IPlist.row(item))
+
+	def updateList(self, old, new):
+		items = self.IPlist.findItems(old, QtCore.Qt.MatchExactly)
+		for item in items:
+			item.setText(new)
 
 if __name__ == '__main__':
 	host = input('Input IP address: ')
@@ -284,11 +328,13 @@ if __name__ == '__main__':
 	dic[host] = 'Server'
 	dic['127.0.0.1'] = 'Server'
 	try:
-		connect(host, port)
+		processText.connect(host, port)
 	except ConnectionRefusedError:
 		print('Cannot connect to the server.')
 	except TimeoutError:
 		print('Connection time out.')
+	except OSError:
+		print('Invalid operation.')
 	else:
 		app = QtWidgets.QApplication(sys.argv)
 		app.setStyle('Fusion')
